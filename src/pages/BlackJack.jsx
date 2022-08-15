06aaa3022cf4f436fb98b543be8aef8c15e54fad
moshe <themoshe4u@gmsil.com>
2022-08-16 01:12:25 +0300

    hit function, messages

--- src/pages/BlackJack.jsx
+++ src/pages/BlackJack.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { generateDeck, dealCards } from "../cmps/DeckFunctions";
+import { generateDeck, dealCards, getRandomCard, getCount } from "../cmps/DeckFunctions";
 import Card from "../cmps/Card";
 
 class BlackJack extends React.Component {
@@ -11,6 +11,7 @@ class BlackJack extends React.Component {
         wallet: 1000,
         message: null,
         inputValue: '',
+        gameStatus: null,
     };
 
     componentWillMount = () => {
@@ -39,25 +40,53 @@ class BlackJack extends React.Component {
         }
     }
 
+    hit() {
+        if (!this.state.gameOver) {
+            if (this.state.currentBet) {
+                const { randomCard, updatedDeck } = getRandomCard(this.state.deck);
+                const player = this.state.player;
+                player.cards.push(randomCard);
+                player.count = getCount(player.cards);
+
+                if (player.count > 21) {
+                    this.setState({ player, gameOver: true, message: 'BUST!' });
+                } else {
+                    this.setState({ deck: updatedDeck, player });
+                }
+            } else {
+                this.setState({ message: 'Please place bet.' });
+            }
+        } else {
+            this.setState({ message: 'Game over! Please start a new game.' });
+        }
+    }
+
     inputChange(e) {
         const inputValue = +e.target.value;
         if (!isNaN(inputValue)) this.setState({ inputValue });
     }
 
     render() {
-        const { dealer, player } = this.state;
+        const { dealer, player, currentBet, inputValue, message } = this.state;
         return (
             <div className="game">
                 <main className="game-container">
                     <h1>BlackJack</h1>
                     <p>Wallet: ${this.state.wallet}</p>
-                    <div className="message-container">{this.state.message}</div>
-                    <div className="input-bet">
-                        <form>
-                            <input type="text" name="bet" placeholder="Enter Your Bet" value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
-                        </form>
-                        <button onClick={() => { this.placeBet() }}>Place Bet</button>
-                    </div>
+                    <div className="message-container">{message}</div>
+                    {
+                        !currentBet ?
+                            <div className="input-bet">
+                                <form>
+                                    <input type="text" placeholder="Enter Your Bet" value={inputValue} onChange={this.inputChange.bind(this)} />
+                                </form>
+                                <button onClick={() => { this.placeBet() }}>Place Bet</button>
+                            </div>
+                            : <div>This Round Bet ${currentBet}
+                            <br />
+                            <button onClick={() => {this.hit()}}>Hit</button>
+                            </div>
+                    }
                     <div className="dealer">
                         Dealer's Cards ({dealer.count})<br />
                         {dealer.cards.map((card, i) => {
